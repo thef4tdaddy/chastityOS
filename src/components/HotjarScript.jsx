a0be92bdd614edc4177fff4d515fdb9a72f080a8
thef4tdaddy <github@ed-david.com>
2025-06-05 12:52:26 -0500

    hotjar debugging

--- src/components/HotjarScript.jsx
+++ src/components/HotjarScript.jsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import { Helmet } from 'react-helmet-async';
+// File: src/pages/HotjarScript.jsx
+import { useEffect } from 'react';
 
 const HotjarScript = ({ isTrackingAllowed }) => {
-  if (!isTrackingAllowed) return null;
+  useEffect(() => {
+    if (!isTrackingAllowed || typeof window === 'undefined') return;
 
-  const hotjarScriptContent = `
-    (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:6424571,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    if (!window.hj) {
+      (function(h, o, t, j, a, r) {
+        h.hj = h.hj || function() {
+          (h.hj.q = h.hj.q || []).push(arguments);
+        };
+        h._hjSettings = { hjid: 6424571, hjsv: 6 };
+        a = o.getElementsByTagName('head')[0];
+        r = o.createElement('script'); r.async = 1;
+        r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
         a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-  `;
+      })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
 
-  return (
-    <Helmet>
-      {/* Change to use dangerouslySetInnerHTML */}
-      <script dangerouslySetInnerHTML={{ __html: hotjarScriptContent }} />
-    </Helmet>
-  );
+      console.log('[Hotjar Debug] Injected Hotjar script.');
+    } else {
+      console.log('[Hotjar Debug] Hotjar already initialized.');
+    }
+  }, [isTrackingAllowed]);
+
+  return null;
 };
 
 export default HotjarScript;
