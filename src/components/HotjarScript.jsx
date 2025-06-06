d6433a96a32340273d69caaf858f26bff4be2041
thef4tdaddy <github@ed-david.com>
2025-06-06 10:58:19 -0500

    hotjar fix

--- src/components/HotjarScript.jsx
+++ src/components/HotjarScript.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
+import { Helmet } from 'react-helmet-async';
 
 const HotjarScript = ({ isTrackingAllowed }) => {
   if (!isTrackingAllowed) return null;
