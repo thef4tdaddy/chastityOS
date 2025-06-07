52bf980fc8e79c41eb431a49bb6ba219fc54cdeb
thef4tdaddy <github@ed-david.com>
2025-06-07 12:34:48 -0500

    Sentry

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),
+  plugins: [
+    react(), 
+    tailwindcss(),
+    // The Sentry Vite plugin must be after all other plugins
+    sentryVitePlugin({
+      org: process.env.SENTRY_ORG,
+      project: process.env.SENTRY_PROJECT,
+      // Auth tokens can be obtained from https://sentry.io/settings/account/api/auth-tokens/
+      // and need `project:releases` and `org:read` scopes
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    }),
   ],
+  build: {
+    // This is important for Sentry to be able to map errors to your source code.
+    sourcemap: true,
+  }
 })
