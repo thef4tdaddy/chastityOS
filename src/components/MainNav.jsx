78b13da2fa7b7730d97bf317285bdbd1611490b5
thef4tdaddy <github@ed-david.com>
2025-06-09 12:19:42 -0500

    Move keyholder section to its own page

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -6,6 +6,7 @@ const MainNav = ({ currentPage, setCurrentPage }) => {
     { id: 'tracker', name: 'Chastity Tracker' },
     { id: 'logEvent', name: 'Log Event' },
     { id: 'fullReport', name: 'Full Report' },
+    { id: 'keyholder', name: 'Keyholder' },
     { id: 'rewards', name: 'Rewards/Punishments' },
     { id: 'settings', name: 'Settings' },
     // { id: 'privacy', name: 'Privacy' }, // Privacy removed from main navigation
