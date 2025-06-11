981aabedbad35b8c60608276d5758de57c31d50b
thef4tdaddy <github@ed-david.com>
2025-06-11 16:15:41 -0500

    changing order of things

--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -1,18 +1,21 @@
 // src/pages/SettingsPage.jsx
 import React from 'react';
-import AccountSection from '../components/settings/AccountSection';
-import DataManagementSection from '../components/settings/DataManagementSection';
-import SessionEditSection from '../components/settings/SessionEditSection';
+import AccountSection from '../components/settings/AccountSection.jsx'; // Added .jsx
+import DataManagementSection from '../components/settings/DataManagementSection.jsx'; // Added .jsx
+import SessionEditSection from '../components/settings/SessionEditSection.jsx'; // Added .jsx
+import DisplaySettingsSection from '../components/settings/DisplaySettingsSection.jsx'; // Added .jsx
 
 const SettingsPage = (props) => {
-  // Pass all props down to each section using the spread operator.
-  // This is a robust way to ensure all necessary data and handlers are available
-  // to the components that need them, resolving the login display issue.
   return (
     <div className="p-0 md:p-4">
+      {/* 1. Profile */}
       <AccountSection {...props} />
-      <DataManagementSection {...props} />
+      {/* 2. Display Settings */}
+      <DisplaySettingsSection {...props} />
+      {/* 3. Edit Chastity Time */}
       <SessionEditSection {...props} sessionId={props.userId} />
+      {/* 4. Data Management */}
+      <DataManagementSection {...props} />
     </div>
   );
 };
