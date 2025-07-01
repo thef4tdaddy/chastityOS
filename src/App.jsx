b0dfa74638a9cba024dfa3d4c148d8b54919752b
thef4tdaddy <github@ed-david.com>
2025-07-01 18:01:41 -0500

    feat: add "How to Use" button to WelcomeModal

--- src/App.jsx
+++ src/App.jsx
@@ -40,6 +40,7 @@ function App() {
           isOpen={!hasAccepted}
           onAccept={accept}
           onShowLegal={() => setShowEulaModal(true)}
+          onShowHowTo={() => setShowHowToModal(true)}
         />
 
         <HowToModal
